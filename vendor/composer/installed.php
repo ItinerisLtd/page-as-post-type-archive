11afb758417f5d887673895a9bb26c48d6fe7b97
Dan Lapteacru <danlapteacru@gmail.com>
2022-11-11 13:19:36 +0200

    add vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a51bfc8207612a1af086702dd1f911168dbf2a94',
+        'name' => 'itinerisltd/page-as-post-type-archive',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'itinerisltd/page-as-post-type-archive' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a51bfc8207612a1af086702dd1f911168dbf2a94',
+            'dev_requirement' => false,
+        ),
+        'roave/security-advisories' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'metapackage',
+            'install_path' => NULL,
+            'aliases' => array(),
+            'reference' => '4ed057f00e70bf1a45434fbc6fe14790684ff3c5',
+            'dev_requirement' => true,
+        ),
+    ),
+);
